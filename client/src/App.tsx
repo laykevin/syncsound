decd978465b783ecc5dccabe4f54e5d72f8a225e
Adison <adisonlay@gmail.com>
2023-05-27 12:55:49 -0700

    Pass query param to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Room, Player } from './components';
 import { Start } from './pages';
-// import { io } from 'socket.io-client';
 
-// const socket = io('http://localhost:7000');
-// socket.on('connect', () => {
-//   displayMessage(`Connected with ${socket.id}`)
-// })
+interface AppProps {
+  roomName: string | null;
+}
 
-const App: React.FC = () => {
+const App: React.FC<AppProps> = () => {
   return (
     <>
       <Start />
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const queryStringParams = new URLSearchParams(window.location.search);
+const roomName = queryStringParams.get('room');
+
 const $root = document.getElementById('root');
 if ($root) {
   const root = ReactDOM.createRoot($root);
   root.render(
     <React.StrictMode>
-      <App />
+      <App roomName={roomName} />
     </React.StrictMode>
   );
 }
