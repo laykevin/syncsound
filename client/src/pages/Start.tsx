31366e8efc4e152c147d78be82c40067cb523f53
Adison <adisonlay@gmail.com>
2023-05-26 22:48:15 -0700

    Set up elements and base styling for Start page

--- client/src/pages/Start.tsx
+++ client/src/pages/Start.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FlexColCenter = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Heading = styled.h1`
+  font-family: sans-serif;
+  color: darkgreen;
+  margin-bottom: 4rem;
+`;
+
+const Panel = styled(FlexColCenter)`
+  border: 1px solid black;
+  border-radius: 4px;
+  padding: 1rem;
+`;
 
 export const Start: React.FC = () => {
   return (
-    <div>
-      <h1>SyncSound</h1>
-      <div>Create Room</div>
-      <div>Join Room</div>
-    </div>
+    <FlexColCenter>
+      <Heading>SyncSound</Heading>
+      <Panel>
+        <h4>Welcome</h4>
+        <label>Room:</label>
+        <input />
+        <label>Name:</label>
+        <input />
+        <div>
+          <button>Create Room</button>
+          <button>Join Room</button>
+        </div>
+      </Panel>
+    </FlexColCenter>
   );
 };
