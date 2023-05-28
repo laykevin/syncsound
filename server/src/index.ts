30f392cd1a7c4de02ecb18a443066ca3ff7f3866
Adison <adisonlay@gmail.com>
2023-05-28 11:35:13 -0700

    Add room routes in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,49 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+const path = require('path');
 import { IVideoData } from '../../client/src/components';
 
-const io = require('socket.io')(7000, {
+const PORT = 3000;
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
   cors: {
-    origin: ['http://localhost:9000'],
+    origin: [`http://localhost:${PORT}`],
   },
+});
+
+const outputDir = path.join(__dirname, '../../client/dist');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(outputDir));
+
+// type IRoom = { room: string }
+// const rooms: IRoom = {} as IRoom;
+const rooms: any = {};
+
+app.get('/', (req, res) => {
+  console.log('from get /');
+  res.sendFile(path.join(outputDir, 'index.html'));
+});
+
+// app.post('/room', (req: Request<{},{},IRoom>, res: Response) => {
+app.post('/room', (req, res) => {
+  console.log('from post /room', req.body);
+  if (rooms[req.body.room] == null) {
+    rooms[req.body.room] = { users: {} };
+  }
+  res.redirect(req.body.room);
+  // Send message that new room was created
+  io.emit('room-created', req.body.room);
+});
+
+app.get('/:room', (req, res) => {
+  console.log('from get /:rooms', rooms);
+  if (rooms[req.params.room] == null) {
+    return res.redirect('/');
+  }
+  res.redirect('/' + '?room=' + req.params.room);
 });
 
 io.on('connection', (socket: any) => {
@@ -18,8 +57,6 @@ io.on('connection', (socket: any) => {
   });
 });
 
-// const app = express();
-
-// app.listen(8080, () => {
-//   console.log('Listening on port 8080');
-// });
+httpServer.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
